9d00d7226767d1f4a0860a5765452c24f1dd35a6
max-bader <mibader@uci.edu>
2025-04-09 19:38:40 -0700

    Successfully created the add task feature which is saved in the task.json file.

---
+++ file.py
@@ -0,0 +1,81 @@
+import sys
+import json
+import os
+
+def load_tasks():
+    """If there are already tasks within the json file, load them or return an empty list"""
+    if os.path.exists("task.json"):
+        with open("task.json", "r") as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return []
+    return []
+
+def save_tasks(tasks):
+    """Save the list of tasks to the json file"""
+    with open("task.json", "w") as file:
+        json.dump(tasks, file)
+
+def add_task(desc):
+    """Add a task to the json file using other created functions"""
+    tasks = load_tasks()
+
+    # get the id for the next task 
+    # each task will have an id assinged to it for easy access
+    if tasks:
+        new_id = tasks[-1]["id"] + 1
+    else:
+        new_id = 1
+
+    # format for json
+    new_task = {
+        "id": new_id,
+        "description": desc
+    }
+
+    # add task to the list of tasks and then save by dumping
+    tasks.append(new_task)
+    save_tasks(tasks)
+    print(f"Task added successfully (ID: {new_id})")
+
+def handle_command(command, arguments):
+    """Handles commands that could be used (add, delete, update) and more"""
+    if command == "add":
+        if arguments:
+            description = " ".join(arguments)
+            add_task(description)
+        else:
+            print("nothing was added; consider the correct format")
+    elif command == "update":
+        if arguments:
+            description = " ".join(arguments)
+            # implement update
+        else:
+            print("task was not updated; see readme for instructions on how to update")
+    elif command == "delete":
+        if arguments:
+            description = " ".join(arguments)
+            # implement delete
+        else:
+            print("nothing was deleted")
+    else:
+        print(f"Unknown command: {command}")
+
+def parse_command():
+    """Parses the command line by using sys.argv. The first string should be 
+    the command followed by its arguments"""
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        arguments = sys.argv[2:]
+        return command, arguments
+    else:
+        print("no commands to parse")
+        sys.exit(1)
+
+def main():
+    command, arguments = parse_command()
+    handle_command(command, arguments)
+    
+if __name__ == "__main__":
+    main()
